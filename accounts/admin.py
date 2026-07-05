be8cd643a2c400b5e7a494ec3f2a613384a7754e
ABOLFAZL <abolfazlmirzaie.1385@gmail.com>
2026-07-05 23:02:02 +0330

    show phone_number in user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+
+class UserAdmin(BaseUserAdmin):
+    BaseUserAdmin.fieldsets += (
+        ("phone_info", {'fields': ('phone_number',)}),
+    )
+    list_display = ('username', 'email', 'phone_number', 'is_staff')
+
 
 
 
