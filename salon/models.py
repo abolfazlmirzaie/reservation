813ee92112e6a6a39822c617dabc1a783405dd30
ABOLFAZL <abolfazlmirzaie.1385@gmail.com>
2026-07-06 04:50:55 +0330

    add stylist model

--- salon/models.py
+++ salon/models.py
@@ -34,3 +34,55 @@ class Salon(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+
+
+
+class Stylist(models.Model):
+    salon = models.ForeignKey(Salon, on_delete=models.CASCADE, related_name='staylists')
+    name = models.CharField(max_length=100)
+    name_en = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    phone = models.CharField(max_length=11)
+    booking_window_days = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name_en)
+            slug = base_slug
+            counter = 1
+            while Salon.objects.filter(slug=slug).exists():
+                slug = f'{base_slug}-{counter}'
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name}-{self.salon.name}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
