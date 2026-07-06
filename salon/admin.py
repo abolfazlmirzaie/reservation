f60e10b5ac9693c2b274b34ef3d34b37ca9dbde0
ABOLFAZL <abolfazlmirzaie.1385@gmail.com>
2026-07-06 04:05:08 +0330

    customize salon admin panel

--- salon/admin.py
+++ salon/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Salon
 
-# Register your models here.
+
+@admin.register(Salon)
+class SalonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'owner', 'created_at', 'is_active')
+    list_filter = ('is_active', 'category')
+    search_fields = ('name', 'name_en', 'phone')
+    readonly_fields = ('created_at', 'slug')
