f6e989803a7fcac581932ccee81735c41242a046
ABOLFAZL <abolfazlmirzaie.1385@gmail.com>
2026-07-10 18:40:07 +0330

    add appointment with custom admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'stylist_service', 'start_time', 'status')
+    list_filter = ('status', 'stylist_service__stylist')
+    search_fields = ('customer_name', 'customer_phone')
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils import timezone
+from salon.models import StylistService
 
-# Create your models here.
+
+
+
+
+
+
+class Appointment(models.Model):
+    STATUS_CHOICES = (
+        ('pending_payment', 'در انتظار پرداخت'),
+        ('confirmed', 'تایید شده'),
+        ('cancelled_by_customer', 'لغو شده توسط مشتری'),
+        ('cancelled_by_stylist', 'لغو شده توسظ ارایشگر'),
+        ('completed', 'انجام شده'),
+        ('no_show', 'عدم حضور مشتری'),
+    )
+
+    stylist_service = models.ForeignKey(StylistService, on_delete=models.PROTECT, related_name='appointments')
+    customer_name = models.CharField(max_length=100)
+    customer_phone = models.CharField(max_length=11)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, default='pending_payment')
+
+
+    service_price_snapshot = models.PositiveIntegerField()
+    deposit_amount = models.PositiveIntegerField()
+    platform_share = models.PositiveIntegerField()
+    salon_share = models.PositiveIntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "نوبت"
+        verbose_name_plural = "نوبت ها"
+
+
+    def __str__(self):
+        return f"{self.customer_name} - {self.stylist_service.stylist.name} - {self.start_time.strftime('%Y-%m-%d %H:%M')}"
