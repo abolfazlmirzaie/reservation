ffaacb1b9cce8d8d09095911a9963c4c19da3545
ABOLFAZL <abolfazlmirzaie.1385@gmail.com>
2026-08-10 17:54:31 +0330

    install and configurate drf-spectacular

--- reservation/settings.py
+++ reservation/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     "accounts.apps.AccountsConfig",
     "salon.apps.SalonConfig",
     "booking.apps.BookingConfig",
+
+
     "rest_framework",
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -88,6 +91,23 @@ REST_FRAMEWORK = {
         "anon": "5/minutes",
         "user": "10/minutes",
         "login": "30/minutes",
+    },
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+
+
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Reservation API",
+    "DESCRIPTION": "API documentation for the reservation platform",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "persistAuthorization": True,
+        "displayOperationId": True,
     },
 }
 
--- reservation/urls.py
+++ reservation/urls.py
@@ -17,11 +17,41 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+    path(
+        "api/schema/",
+        SpectacularAPIView.as_view(),
+        name="schema",
+    ),
+
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(
+            url_name="schema"
+        ),
+        name="swagger-ui",
+    ),
+
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(
+            url_name="schema"
+        ),
+        name="redoc",
+    ),
+
+
+
     path("admin/", admin.site.urls),
     path("", include("salon.urls")),
     path("api/auth/", include("accounts.urls")),
