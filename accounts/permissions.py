751c3c23079b44dbada86a1a17e1984efe1ceecf
Abolfazl Mirzaie <abolfazlmirzaie.1385@gmail.com>
2026-08-18 12:37:06 +0330

    feat/ create a custom permission for manage appointment by customer

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -3,6 +3,8 @@ from functools import cache
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import BasePermission, IsAuthenticated
+
+from booking.models import Appointment
 from salon.models import Stylist
 
 User = get_user_model()
@@ -32,3 +34,19 @@ class IsCustomer(BasePermission):
     def has_permission(self, request, view):
 
         return request.user.is_authenticated and request.user.role == User.Role.CUSTOMER
+
+
+
+
+
+class CanManageAppointments(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated or not request.user.role == User.Role.CUSTOMER:
+            return False
+        appointment = get_object_or_404(Appointment, id=view.kwargs['appointment_id'])
+
+        if appointment.customer_phone == request.user.phone_number:
+            return True
+        return False
+
+
