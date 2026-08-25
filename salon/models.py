6bea0b96f672ece3906f988cf7caca4a8979f2d7
Abolfazl Mirzaie <abolfazlmirzaie.1385@gmail.com>
2026-08-25 22:37:10 +0330

    fix: fix Service model

--- salon/models.py
+++ salon/models.py
@@ -78,12 +78,11 @@ class Stylist(models.Model):
 
 
 class Service(models.Model):
-    salon = models.ForeignKey(Salon, on_delete=models.CASCADE, related_name="services")
     name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.name} - {self.salon.name}"
+        return self.name
 
 
 class StylistService(models.Model):
