1e6bdd3a66ce739338531f0fca8eca0a6b984722
abolfazl <abolfazlmirzaie.dev@gmail.com>
2026-09-20 20:58:03 +0330

    feat: configure settings.py for send request by front-end

--- reservation/settings.py
+++ reservation/settings.py
@@ -59,9 +59,11 @@ INSTALLED_APPS = [
     "drf_spectacular",
     "silk",
     "rest_framework_simplejwt.token_blacklist",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware"
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -144,6 +146,16 @@ DATABASES = {
     }
 }
 
+
+# FOR DEVELOPMENT ONLY
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3001",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
