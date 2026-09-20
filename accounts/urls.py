5f0d1b01bef8262c2db4072c6e1bdd433f8a7aa5
abolfazl <abolfazlmirzaie.dev@gmail.com>
2026-09-20 16:21:28 +0330

    feat: register Profile model and profile api

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import OTPVerifyView, RegisterOrLoginView
+from .views import OTPVerifyView, RegisterOrLoginView, ProfileAPIView
 
 urlpatterns = [
     path("request-otp/", RegisterOrLoginView.as_view(), name="request-otp"),
     path("verify-otp/", OTPVerifyView.as_view(), name="verify-otp"),
+    path("profile/", ProfileAPIView.as_view(), name="profile"),
 ]
